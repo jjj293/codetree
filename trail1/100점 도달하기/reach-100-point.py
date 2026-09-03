e41b85088bfaa404d8922b21cfcb921468516238
jjj293 <jm0329@korea.ac.kr>
2026-09-03 18:08:05 +0900

    [Passed] 100점 도달하기 (48ms, 16MB)

---
+++ trail1/100점 도달하기/reach-100-point.py
@@ -0,0 +1,14 @@
+n=int(input())
+score=n
+while score<=100:
+    if score>=90:
+        print("A", end=' ')
+    elif score>=80:
+        print("B", end=' ')
+    elif score>=70:
+        print("C", end=' ')
+    elif score>=60:
+        print("D", end=' ')
+    else:
+        print("F", end=' ')
+    score+=1
