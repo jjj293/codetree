7b3e4b26a49aa8053a89c007236204e3a83e509f
jjj293 <jm0329@korea.ac.kr>
2026-09-05 15:44:03 +0900

    [Passed] 정사각형 두 개 출력 (51ms, 16MB)

--- trail1/정사각형 두 개 출력/output-two-rectangles.py
+++ trail1/정사각형 두 개 출력/output-two-rectangles.py
@@ -1,10 +1,7 @@
 n=int(input())
-for _ in range(n):
+for _ in range(2):
     for _ in range(n):
-        print("*", end='')
-    print()
-print()
-for _ in range(n):
-    for _ in range(n):
-        print("*", end='')
+        for _ in range(n):
+            print("*", end='')
+        print()
     print()
